098b9dcbea1ec58afdc5b111a4f669e09ad4e5fb
Simon Eddy <simon@simoneddy.com.au>
2019-05-11 16:22:41 +1000

    removed unused import

--- lib/console/src/Commands/DrawSherrin.php
+++ lib/console/src/Commands/DrawSherrin.php
@@ -2,7 +2,6 @@
 namespace Phooty\Console\Commands;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
