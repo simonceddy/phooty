7a9ccf3ffdef614d6f404a0e9f16e6e78b2edb36
Simon Eddy <simon@simoneddy.com.au>
2019-06-13 14:19:32 +1000

    more playing with react loop

--- index.php
+++ index.php
@@ -3,26 +3,39 @@ require 'vendor/autoload.php';
 require 'autoload.php';
 $app = include_once 'bootstrap.php';
 
-/* $player = new Phooty\Orm\Entities\Player();
-$player->setSurname('jiffy'); */
-
-/* $map = new Phooty\Core\Tilemap\Tilemap(120, 180);
-
-$tile = $map->tile(100, 78);
-$tile->addMovableEntity($mp = new MovableEntityWrapper($player));
-//dd($tile->hasEntity($mp));
-dd($map); */
-
-//dd((new EntityToArray())->convert($player));
-
-//$app->make(Phooty\Core\Simulation::class)->run();
 $kernel = $app->make(Phooty\Console\Kernel::class);
-$kernel->run();
-//dd($app);
 
-//$knl = new Phooty\Simulation\Kernel();
-//dd($loop, memory_get_usage());
+//$kernel->run();
 
-/* $kernel->getHelperSet()->set($app->make(EntityManagerHelper::class));
+$loop = React\EventLoop\Factory::create();
 
-$kernel->run(); */
+$loop->addPeriodicTimer(10, function () {
+    $memory = memory_get_usage() / 1024;
+    $formatted = number_format($memory, 3).'K';
+    echo "Current memory usage: {$formatted}\n";
+});
+
+$stdout = new React\Stream\WritableResourceStream(STDOUT, $loop);
+$stdin = new React\Stream\ReadableResourceStream(STDIN, $loop);
+$stdin->pipe($stdout);
+
+
+$stdin->on('data', function ($data) {
+    $data = new Symfony\Component\Console\Input\StringInput($data);
+    
+    switch ((string) $data) {
+        case 'sim':
+            $sim = new Phooty\Simulation\Kernel();
+            dd($sim);
+            break;
+        default:
+            echo 'data' . PHP_EOL;
+    }
+});
+
+echo (new Symfony\Component\Console\Formatter\OutputFormatter())->format(
+    Phooty\Console\Artwork\TitleLarry3D::render()
+);
+
+
+$loop->run();
